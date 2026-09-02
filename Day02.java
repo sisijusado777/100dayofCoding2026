50c299f4b9c33b56bb36eb366b508ba47fd99441
sisijusado777 <sisijusad@gmail.com>
2026-09-03 01:19:16 +0800

    Create Day02.java
    belajar java day 02

---
+++ Day02.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String[] args) {
+
+        //Menampilkan biodata menggunakan println()
+        System.out.println("BIODATA DIRI");
+
+        System.out.println("Nama    : Sisi Jusado");
+        System.out.println("NIM     : D0224039");
+
+        //Menampilkan biodata menggunakan print()
+        System.out.println("Jurusan : ");
+        System.out.println("Teknik Informatika\n");
+
+        //menampilkan biodata menggunakan printf()
+        System.out.println("Umur     : %d tahun%n",19");
+    }
+}
