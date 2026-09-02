61bcf2a02412d429f7c510190a1db928e9750f91
sisijusado777 <sisijusad@gmail.com>
2026-09-03 00:37:06 +0800

    Create Day01.java
    day01

---
+++ Day01.java
@@ -0,0 +1,25 @@
+// Ini adalah komentar satu baris
+/*
+   Ini adalah komentar
+   yang terdiri dari
+   beberapa baris
+*/
+/**
+ * Ini adalah komentar dokumentasi Java.
+ * Biasanya digunakan untuk menjelaskan class atau method.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Menampilkan tulisan ke layar
+        System.out.println("Halo, saya sedang belajar Java!");
+
+        /*
+         * Komentar bisa digunakan untuk
+         * menjelaskan fungsi dari kode.
+         */
+
+        System.out.println("Hari 1 - Belajar Java");
+    }
+}
