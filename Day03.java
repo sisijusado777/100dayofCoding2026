463d9a7158b270acc08a47bcf93c9a4eee3f7f2e
sisijusado777 <sisijusad@gmail.com>
2026-09-04 05:48:36 +0800

    Create Day03.java
    hari ke 3

---
+++ Day03.java
@@ -0,0 +1,23 @@
+public class day03 {
+    public static void main(String[] args) {
+
+        //menggunakan \n untuk membuat baris baru
+        System.out.println("Nama    : Sisi Jusado\nNIM     : D0224039");
+
+        //menggunakan \t untuk membuat jarak seperti tab
+        System.out.println("Nama\t: Sisi Jusado");
+        System.out.println("NIM\t: D0224039");
+
+        //menggunakan \" untuk menampilkan tanda petik
+        System.out.println("Dia berkata, \"Saya sedang ngoding.\"");
+
+        //menggunakan \\ untuk menampilkan tanda backlash
+        System.out.println("Lokasi file: C:\\Users\\lenovo\\Documents\\day100" );
+
+        //menggunakan \b untuk menghapus satu karakter sebelumnya
+        System.out.println("Javva\b");
+
+        //menggunakan \r untuk kembali ke awal baris
+        System.out.println("Belajar\rJava");
+    }
+}
