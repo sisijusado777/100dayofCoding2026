89414ac874303d7af213ed3c6c0d6da138a1d204
sisijusado777 <sisijusad@gmail.com>
2026-09-05 09:05:38 +0800

    Create Day04.java
    hari ke 4

---
+++ Day04.java
@@ -0,0 +1,31 @@
+public class day04 {
+    public static void main(String[] args) {
+
+        //ini tipe data byte
+        byte umur = 19;
+
+        //ini tipe data short
+        short tahunLahir = 2007;
+
+        //ini tipe data int
+        int nim = 224039;
+
+        //ini tipe data long
+        long jumlahPenduduk = 2900000000L;
+
+        //menampilkan judul program
+        System.out.println("DATA NUMERIK BILANGAN BULAT");
+
+        //menampilkan nilai variabel Byte
+        System.out.println("Byte    (umur)              : " + umur);
+
+        //menampilkan nilai variabel Short
+        System.out.println("Short   (tahun lahir)       : " + tahunLahir);
+
+        //menampilkan nilai variabel Int
+        System.out.println("Int     (NIM)               : " + nim);
+
+        //menampilkan nilai variabel Long
+        System.out.println("Long    (jumlah penduduk)   : " + jumlahPenduduk);
+    }
+}
