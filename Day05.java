955577b0cd2a8910c183f8599b4ca95943c67fa8
sisijusado777 <sisijusad@gmail.com>
2026-09-06 19:27:33 +0800

    Create Day05.java
    hari ke 5

---
+++ Day05.java
@@ -0,0 +1,20 @@
+public class day05 {
+    public static void main(String[] args) {
+
+        //membuat variabel dengan tipe data float untuk menyimpan nilai pecahan 
+        //huruf f digunakan untuk menandakan bahwa nilai tersebut bertipe float
+        float tinggiBadan = 165.5f;
+
+        //membuat variabel dengan tipe data double untuk menyimpan nilai pecahan
+        double beratBadan = 55.75;
+
+        //menampilkan judul program
+        System.out.println("TIPE DATA PECAHAN : ");
+
+        //menampilkan nilai dari variabel float
+        System.out.println("Tinggi badan (float)    : " + tinggiBadan);
+
+        //menampilkan nilai dari variabel double
+        System.out.println("Berat badan (double)    : " + beratBadan);
+    }
+}
