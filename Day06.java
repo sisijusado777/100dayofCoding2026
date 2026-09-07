f7d1457604029f9d3da8c9b656dd1eeebc22d543
sisijusado777 <sisijusad@gmail.com>
2026-09-07 18:06:19 +0800

    Create Day06.java
    hari ke 6

---
+++ Day06.java
@@ -0,0 +1,33 @@
+public class day06 {
+    public static void main(String[] args) {
+
+        //membuat satu karakter menggunakan tipe data char
+        char jenisKelamin = 'L';
+
+        //membuat variabel dengan tipe data char untuk menyimpan satu huruf
+        char nilai = 'A';
+
+        //membuat variabel dengan tpe data boolean untuk menyimpan nilai benar/salah
+        boolean mahasiswa = true;
+
+        //membuat variabel boolean untuk menunjukkan apakah sudah lulus
+        boolean lulus = false;
+
+
+        //menampilkan judul program
+        System.out.println("DATA CHAR DAN BOOLEAN");
+
+        //menampilkan nilai dari jenis kelamin
+        System.out.println("Jenis Kelamin (char)    : " + jenisKelamin);
+
+        //menampilkan hasil dari variabel  char nilai
+        System.out.println("Nilai (char)            : " + nilai);
+
+        //menampilkan variabel dari boolean mahasiswa
+        System.out.println("Mahasiswa (boolean)     : " + mahasiswa);
+
+        //menampilkan variabel dari boolean lulus
+        System.out.println("Lulus (boolean)         : " + lulus);
+    }
+    
+}
