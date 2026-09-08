223edff13ac934998dcba139ba0b75a6f84df359
sisijusado777 <sisijusad@gmail.com>
2026-09-08 19:11:32 +0800

    Create Day07.java
    hari ke 7

---
+++ Day07.java
@@ -0,0 +1,31 @@
+public class day07 {
+    public static void main(String[] ARGS) {
+
+        //membuat variabel string untuk menyimpan nama
+        String nama = "Sisi Jusado";
+
+        //membuat variabel string untuk menyimpan NIM
+        String nim = "D0224039";
+
+        //membuat variabel String untuk menyimpan jurusan
+        String jurusan = "Teknik Informatika";
+
+        //membuat variabel String untuk membuat alamat
+        String alamat = "Majene";
+
+        //menampilkan judul
+        System.out.println(" DATA STRING :");
+
+        //Menampilkan nilai nama
+        System.out.println("Nama        : " + nama);
+
+        //menampilkan nilai nim
+        System.out.println("NIM         : " + nim);
+
+        //menampilkan nilai jurusan
+        System.out.println("Jurusan     : " + jurusan);
+
+        //menampilkan nilai alamat
+        System.out.println("Alamat      : " + alamat);
+    }
+}
