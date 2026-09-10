f41a7d9883f408d6b19c9e33d94fca4ab2eaad7b
sisijusado777 <sisijusad@gmail.com>
2026-09-10 20:15:10 +0800

    Create Day09.java
    hari ke 9

---
+++ Day09.java
@@ -0,0 +1,28 @@
+public class day09 {
+    public static void main(String[] args) {
+        //membuat konstanta dengan tipe data string
+        //kata kunci final membuat nilai variabel tidak dapat di ubah lagi
+        final String nama = "Sisi Jusado";
+
+        // membuat konstanta dengan tipe data int untuk menyimpan NIM
+        final int nim = 224039;
+
+        //mebuat konstanta dengan tipe data double untuk menyimpan nilai ipk
+        final double ipk = 3.55;
+
+        //menampilkan judul
+        System.out.println("DATA KONSTANTA");
+
+        //menampilkan hasil konstanta nama
+        System.out.println("Nama    : " + nama);
+
+        //menampilkan hasil konstanta nim
+        System.out.println("NIM     : " + nim);
+
+        System.out.println("IPK     : " + ipk);
+
+        // nilai konstanta tidak dapat diubah
+    }
+
+    
+}
