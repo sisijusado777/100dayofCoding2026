8a6e7ca3fa0ff52a8088cede4b3b2022fb139457
sisijusado777 <sisijusad@gmail.com>
2026-09-11 19:08:09 +0800

    Create Day10.java
    hari ke 10

---
+++ Day10.java
@@ -0,0 +1,44 @@
+public class day10 {
+    public static void main(String[] args) {
+
+        //menampilkan judul
+        System.out.println("BATAS MINIMAL DAN MAKSIMAL TIPE DATA : ");
+
+        //menampilkan nilai minimum dari tipe data byte
+        System.out.println("Byte minimum    : " + Byte.MIN_VALUE);
+
+        //menampilkan nilai maksimum dari tipe data byte
+        System.out.println("Byte maksimum    : " + Byte.MAX_VALUE);
+
+        //menampilkan nilai minimum dari tipe data short
+        System.out.println("Short maksimum    : " + Short.MAX_VALUE);
+
+        //menampilkan nilai maksimum dari tipe data short
+        System.out.println("Byte maksimum    : " + Short.MAX_VALUE);
+
+        //menampilkan nilai minimum dari tipe data integer
+        System.out.println("Integer maksimum    : " + Integer.MAX_VALUE);
+
+        //menampilkan nilai maksimum dari tipe data integer
+        System.out.println("Integer maksimum    : " + Integer.MAX_VALUE);
+
+        //menampilkan nilai minimum dari tipe data long
+        System.out.println("Long minimum        : " + Long.MAX_VALUE);
+
+        //menampilkan nilai maksimum dari tipe data long
+        System.out.println("Long maksimum       : " + Long.MAX_VALUE);
+
+        //menampilkan nilai minimum dari tipe data float
+        System.out.println("Float minimum       : " + Float.MAX_VALUE);
+
+        //menampilkan nilai maksimum dari tipe data float
+        System.out.println("Float maksimum      : " + Float.MAX_VALUE);
+
+        //menampilkan nilai minimum dari tipe data double
+        System.out.println("Double minimum      : " + Double.MAX_VALUE);
+
+        //menampilkan nilai maksimum dari tipe data double
+        System.out.println("Double maksimum     : " + Double.MAX_VALUE);
+
+    }
+}
