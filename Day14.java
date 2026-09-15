c567efddcd532632b6351299838d6a4624ce1567
sisijusado777 <sisijusad@gmail.com>
2026-09-15 22:08:04 +0800

    Create Day14.java
    hari ke 14

---
+++ Day14.java
@@ -0,0 +1,34 @@
+//membuat class
+public class day014 {
+
+    //membuat method main
+    public static void main(String[] args) {
+        
+        //membuat variabel angka pertama 
+        int angka1 = 20;
+
+        //membuat variabel angka kedua
+        int angka2 = 8;
+
+        //menjumlahkan angka1 dan angka2, kemudian menyimpan hasilnya ke variabel penjumlahan
+        int penjumlahan = angka1 + angka2;
+
+        //mengurangi angka1 dan angka2, kemudian menyimpan hasilnya ke variabel pengurangan
+        int pengurangan = angka1 - angka2;
+
+        //menampilkan judul program
+        System.out.println("Operator Aritmatika");
+
+        //menampilkan nilai angka1
+        System.out.println("Angka Pertama       : " + angka1);
+
+        //menampilkan nilai angka2
+        System.out.println("Angka Kedua         : " + angka2);
+
+        //menampilkan hasil penjumlahan
+        System.out.println("Penjumlahan         : " + penjumlahan);
+
+        //menampilkan hasil pengurangan
+        System.out.println("Pengurangan         : " + pengurangan);
+    }
+}
