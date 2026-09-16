95ed865169b5859a55e756ca309b75a0554860d7
sisijusado777 <sisijusad@gmail.com>
2026-09-16 19:12:01 +0800

    Create Day15.java
    hari ke 15

---
+++ Day15.java
@@ -0,0 +1,34 @@
+//membuat class
+public class day015 {
+
+    //membuat method main
+    public static void main(String[] args) {
+        
+        //membuat variabel angka pertama dengan tipe int
+        int angka1 = 30;
+
+        //membuat variabel angka kedua dengan tipe data int
+        int angka2 = 5;
+
+        //mengalikan angka1 dengan angka2 lalu di simpan di variabel perkalian
+        int perkalian = angka1 * angka2;
+
+        //membagi angka1 dengan angka dua dan di simpan di variabel pembagian
+        int pembagian = angka1 / angka2;
+
+        //menampilkan judul
+        System.out.println("Operator Aritmatika");
+
+        //menampilkan nilai dari angka pertama
+        System.out.println("Angka1      = " + angka1);
+
+        //menampilkan nilai dari angka kedua
+        System.out.println("Angka2      = " + angka2);
+
+        //menampilkan hasil perkalian
+        System.out.println("Perkalian   = " + perkalian);
+
+        //menampilkan hasil dari pembagian
+        System.out.println("Pembagian   = " + pembagian);
+    }
+}
