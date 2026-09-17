ef7b28b9b4458a9ba6719e264fdd70ed0a596abd
sisijusado777 <sisijusad@gmail.com>
2026-09-17 20:20:24 +0800

    Create Day16.java
    hari ke 16

---
+++ Day16.java
@@ -0,0 +1,17 @@
+public class day016{
+
+    public static void main(String[] args) {
+        
+        int angka1 = 50;
+        int angka2 = 6;
+
+        int sisaBagi = angka1 % angka2;
+
+        System.out.println("Operator Modulus");
+
+        System.out.println("Angka1      : " + angka1);
+        System.out.println("Angka2       : " + angka2);
+        System.out.println("Sisa Bagi   : " + sisaBagi);
+    }
+
+}
